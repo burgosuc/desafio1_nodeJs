f28af27ce130ac5b8dd5bffaabc548a6ab58166f
Jonathan Lopez Burgos <jonathanlopezburgos@MacBook-Pro-de-Jonathan.local>
2024-01-12 17:09:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { registrar, leer } = require('./operaciones');
+
+const [operacion, nombre, edad, animal, color, enfermedad] =
+  process.argv.slice(2);
+
+if (operacion.toLowerCase() === 'registrar') {
+  registrar(nombre, edad, animal, color, enfermedad);
+} else if (operacion.toLowerCase() === 'leer') {
+  leer();
+} else {
+  console.log(
+    `No existe la operacion "${operacion}". Operaciones existentes "Registrar" y "Leer".`
+  );
+}
